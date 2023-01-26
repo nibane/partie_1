dd03697774f6b6506a5c8aecfccca4fc6e2d4021
nibane <azari46165@gmail.com>
2023-01-26 11:44:00 +0100

    rien

---
+++ exo_15.php
@@ -0,0 +1,11 @@
+<h1>Exercice 15<h/1>
+
+<p>Créer une classe Personne (nom, prénom et date de naissance).Instancier 2 personnes et afficher leurs informations: nom, prénom et âge.$p1 = new Personne("DUPONT","Michel", "1980-02-19");$p2 =new Personne("DUCHEMIN","Alice", "1985-01-17");Affichage:Michel DUPONT a ... ansAlice DUCHEMIN a ... ans <p/>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+
+?>
