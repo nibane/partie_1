e1970bcbd048f5711052738d3263406a5b8c03ae
nibane <azari46165@gmail.com>
2023-02-02 15:45:29 +0100

    Création de la class Personne avec les propriété les setter est getter qui vont avec puis, création de $p1 est de $p2 avec les propriété demander puis , affichage des propriété en utilisant get , ensuite création du constructeur de class puis création de la methode __toString et enfin affichage des propriété en utilisant la methode __tostring combiné au getter et en utilisant uniquement __toString

--- exo_15.php
+++ exo_15.php
@@ -6,13 +6,22 @@
 
 <?php
 
-class personne{
+class Personne{
     public $name;
     public $prenom;
-    public $naissance;
+    private $naissance;
+
     
     //propriété_______________________________
     //méthode_________________________________
+
+    function __construct($name, $prenom, $naissance) {
+        $this->name = $name;
+        $this->prenom = $prenom;
+        $this->naissance = $naissance;
+        
+      }
+
     function set_name($name){
         $this->name = $name;
     }
@@ -37,7 +46,35 @@ class personne{
         return $this->naissance;
     }
 
+    function __toString() {
+        return $this->name . " " . $this->prenom;
+    }
     
 }
+
+$p1 = new personne('DUPONT','Michel','1980-02-19');
+$p2 = new personne('DUCHEMIN','Alice','1985-01-17');
+
+// $p1 = new Personne();
+// $p1->set_name('DUPONT');
+// $p1->set_prenom('Michel');
+// $p1->set_naissance('1980-02-19');
+
+// $p2 = new Personne();
+// $p2->set_name('DUCHEMIN');
+// $p2->set_prenom('Alice');
+// $p2->set_naissance('1985-01-17');
+
+echo $p1->get_name().' ';
+echo $p1->get_prenom().' ';
+echo $p1->get_naissance().'<br>';
+
+echo $p2->get_name().' ';
+echo $p2->get_prenom().' ';
+echo $p2->get_naissance().'<br>';
+
+
+echo $p1 . " est né le  : " . $p1->get_naissance().'<br>';
+echo $p2;
 
 ?>
